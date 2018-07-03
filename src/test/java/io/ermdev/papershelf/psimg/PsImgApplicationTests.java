484abb1445f59de809a5e84ea3204f5f66173fc6
erafaelmanuel <erafaelmanuel@gmail.com>
2018-07-04 03:02:34 +0800

    Initial commit

---
+++ src/test/java/io/ermdev/papershelf/psimg/PsImgApplicationTests.java
@@ -0,0 +1,16 @@
+package io.ermdev.papershelf.psimg;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PsImgApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
